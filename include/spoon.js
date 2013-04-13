626e34a21dadb0a21d2a927a5c4d02efe52800af
FlorentDotMe <florent.bervas@gmail.com>
2013-04-13 19:29:31 +0200

    some fix

--- include/spoon.js
+++ include/spoon.js
@@ -1,10 +1,11 @@
 var email64 = "ZmxvcmVudC5iZXJ2YXNAZ21haWwuY29tCg==";
+var phone64 = "KygzMykgNiA4MSA4NCAzOSAzNAo=";
 
 window.onload = function() {
     document.getElementById("nav-"+document.getElementById("index").className).className = "active";
-    document.getElementById("author-mail").href = 'mailto:'+decodeEmail(email64);
+    document.getElementById("author-mail").href = 'mailto:'+decode64(email64);
 }
 
-function decodeEmail(email) {
-    return base64.decode(email);
+function decode64(obj) {
+    return base64.decode(obj);
 }
